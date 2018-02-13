d30419ce88645347de39bd5d68f12dc433ad7eeb
lewisbaker <lewis.baker@ostmodern.co.uk>
2018-02-13 12:02:24 +0000

    Create ingest_starships

---
+++ shiptrader/management/commands/ingest_starships.py
@@ -0,0 +1,67 @@
+import requests
+
+from testsite.settings import STARSHIP_API_URL
+from django.core.management.base import BaseCommand, CommandError
+from shiptrader.models import Starship
+
+
+class Command(BaseCommand):
+
+    help = 'Ingest latest Starship data'
+
+    def handle(self, *args, **options):
+        url = STARSHIP_API_URL
+
+        response = requests.get(url)
+        next_page = response.json()['next']
+
+        while next_page is not None:
+
+            starships = response.json()['results']
+            next_page = response.json()['next']
+
+            for ship in starships:
+
+                if ship['hyperdrive_rating'] == 'unknown':
+                    ship['hyperdrive_rating'] = 0.0
+                if ship['crew'] == 'unknown':
+                    ship['crew'] = 0
+                if ship['passengers'] == 'unknown':
+                    ship['passengers'] = 0
+                if ship['length'] == 'unknown':
+                    ship['length'] = 0
+                if ship['cargo_capacity'] == 'unknown':
+                    ship['cargo_capacity'] = 0
+
+                if type(ship['hyperdrive_rating']) == str:
+                    ship['hyperdrive_rating'] = ship[
+                        'hyperdrive_rating'].replace(',', '')
+                if type(ship['cargo_capacity']) == str:
+                    ship['cargo_capacity'] = ship[
+                        'cargo_capacity'].replace(',', '')
+                if type(ship['crew']) == str:
+                    ship['crew'] = ship[
+                        'crew'].replace(',', '')
+                if type(ship['passengers']) == str:
+                    ship['passengers'] = ship[
+                        'passengers'].replace(',', '')
+                if type(ship['length']) == str:
+                    ship['length'] = ship[
+                        'length'].replace(',', '')
+
+                Starship.objects.create(
+                    starship_class=ship['model'],
+                    manufacturer=ship['manufacturer'],
+                    length=ship['length'],
+                    hyperdrive_rating=float(ship['hyperdrive_rating']),
+                    cargo_capacity=int(ship['cargo_capacity']),
+                    crew=int(ship['crew']),
+                    passengers=int(ship['passengers'])
+
+                )
+            if not next_page:
+                break
+            response = requests.get(next_page)
+
+        print(len(Starship.objects.all().values()))
+
