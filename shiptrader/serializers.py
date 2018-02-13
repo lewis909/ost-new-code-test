39c120ef0eaed1ef8bf547971b44dc4c85ca84e3
lewisbaker <lewis.baker@ostmodern.co.uk>
2018-02-13 14:31:02 +0000

    Create serializers

---
+++ shiptrader/serializers.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers
+from shiptrader.models import Listing, Starship
+
+
+class StarshipSerializer(serializers.ModelSerializer):
+    
+    id = serializers.IntegerField(read_only=True)
+    starship_class = serializers.CharField(max_length=255)
+    manufacturer = serializers.CharField(max_length=255)
+    length = serializers.FloatField()
+    hyperdrive_rating = serializers.FloatField()
+    cargo = serializers.IntegerField()
+    crew = serializers.IntegerField()
+    passengers = serializers.IntegerField()
+
+    class Meta:
+        model = Starship
+
+        fields = (
+            'id',
+            'starship_class',
+            'manufacturer',
+            'length',
+            'hyperdrive_rating',
+            'cargo',
+            'crew',
+            'passengers'
+
+        )
+
+
+class listingsSerializer(serializers.ModelSerializer):
+
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=255)
+    ship_type = serializers.PrimaryKeyRelatedField()
+    price = serializers.IntegerField()
+
+    class Meta:
+        model = Listing
+        fields = (
+            'id',
+            'name',
+            'ship_type',
+            'price'
+        )
