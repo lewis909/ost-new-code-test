28faa70a9dc5e22e85c81cf0c709230e8a36e199
lewisbaker <lewis.baker@ostmodern.co.uk>
2018-02-15 12:28:28 +0000

    Add migration

--- shiptrader/utlis.py
+++ shiptrader/utlis.py
@@ -4,6 +4,7 @@ from testsite.settings import STARSHIP_API_URL
 from shiptrader.models import Starship
 
 
+# Made this a function so it can be turned into a Celery task.
 def starship_data_ingest():
     url = STARSHIP_API_URL
 
@@ -16,17 +17,17 @@ def starship_data_ingest():
         next_page = response.json()['next']
 
         for ship in starships:
-
+            
             if ship['hyperdrive_rating'] == 'unknown':
-                ship['hyperdrive_rating'] = 0.0
+                ship['hyperdrive_rating'] = None
             if ship['crew'] == 'unknown':
-                ship['crew'] = 0
+                ship['crew'] = None
             if ship['passengers'] == 'unknown':
-                ship['passengers'] = 0
+                ship['passengers'] = None
             if ship['length'] == 'unknown':
-                ship['length'] = 0
+                ship['length'] = None
             if ship['cargo_capacity'] == 'unknown':
-                ship['cargo_capacity'] = 0
+                ship['cargo_capacity'] = None
 
             if type(ship['hyperdrive_rating']) == str:
                 ship['hyperdrive_rating'] = ship[
@@ -57,14 +58,13 @@ def starship_data_ingest():
                 starship_class=ship['model'],
                 manufacturer=ship['manufacturer'],
                 length=ship['length'],
-                hyperdrive_rating=float(ship['hyperdrive_rating']),
-                cargo_capacity=int(ship['cargo_capacity']),
-                crew=int(ship['crew']),
-                passengers=int(ship['passengers'])
+                hyperdrive_rating=ship['hyperdrive_rating'],
+                cargo_capacity=ship['cargo_capacity'],
+                crew=ship['crew'],
+                passengers=ship['passengers']
 
             )
         if not next_page:
             break
         response = requests.get(next_page)
-
-    print(len(Starship.objects.all().values()))
+    print("Number of Ships ingested: ", len(Starship.objects.all().values()))
